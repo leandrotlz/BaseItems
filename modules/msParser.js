4e4723cfaee6e693498b3d82886f4fcd59def961
Leandro <leandrotlz@gmail.com>
2026-09-06 20:36:49 +0200

    Add pstring generator.

--- modules/msParser.js
+++ modules/msParser.js
@@ -12,3 +12,23 @@ export function msParser(rawText) {
     }
     return pstrings;
 };
+
+export function msAddString(pstrings, rawText) {
+    function crc32(str) {
+        var crc = 0xFFFFFFFF;
+        for (var i = 0; i < str.length; i++) {
+            crc ^= str.charCodeAt(i);
+            for (var j = 0; j < 8; j++) {
+                if ((crc & 1) != 0) {
+                    crc = (crc >>> 1) ^ 0xEDB88320;
+                } else {
+                    crc = crc >>> 1;
+                }
+            }
+        }
+        return (crc ^ 0xFFFFFFFF) >>> 0;
+    }
+
+    const key = "P" + crc32(rawText);
+    pstrings[key] = rawText;
+};
