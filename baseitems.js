0b8453582774c0c4d451e0bcd6600cba3cbc790f
Leandro <leandrotlz@gmail.com>
2026-09-06 20:07:35 +0200

    Export JSON file.

--- baseitems.js
+++ baseitems.js
@@ -5,6 +5,7 @@ const dictionaryFileInput = document.getElementById('dictionary-file-input');
 const detailsFileInput = document.getElementById('details-file-input');
 const importDictionaryBtn = document.getElementById('import-dictionary-file');
 const importDetailsBtn = document.getElementById('import-details-file');
+const exportJsonBtn = document.getElementById('export-json-file');
 const jsonPreview = document.getElementById('json-preview');
 
 let pstrings = {}
@@ -26,14 +27,13 @@ dictionaryFileInput.addEventListener('change', (event) => {
 
     reader.onload = (e) => {
         const rawContent = e.target.result;
-        importDetailsBtn.removeAttribute("disabled");
         pstrings = msParser(rawContent);
         jsonPreview.textContent = JSON.stringify(pstrings, null, 2);
+        importDetailsBtn.removeAttribute("disabled");
     };
 
     reader.readAsText(file);
 });
-
 
 detailsFileInput.addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -44,9 +44,23 @@ detailsFileInput.addEventListener('change', (event) => {
     reader.onload = (e) => {
         const rawContent = e.target.result;
         details = texParser(rawContent, ParseDetailDict, pstrings);
-
         jsonPreview.textContent = JSON.stringify(details, null, 2);
+        exportJsonBtn.removeAttribute("disabled");
     };
 
     reader.readAsText(file);
+});
+
+exportJsonBtn.addEventListener('click', () => {
+    const jsonString = JSON.stringify(details, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'Details.json';
+    document.body.appendChild(link);
+    // It's 2026 and we're still faking clicks to trigger a download...
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
 });
