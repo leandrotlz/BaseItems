ce08c6afa1410daa546c416bec400839cef4ee0d
Leandro <leandrotlz@gmail.com>
2026-09-07 17:15:49 +0200

    Add Import JSON button.

--- baseitems.js
+++ baseitems.js
@@ -1,15 +1,26 @@
-import { texParser, ParseDetailDict } from './modules/texParser.js';
+import { texParser, generateFiles, ParseDetailDict } from './modules/texParser.js';
 import { msParser } from './modules/msParser.js';
 import { tabManager } from './modules/tabManager.js';
 
 const dictionaryFileInput = document.getElementById('dictionary-file-input');
 const detailsFileInput = document.getElementById('details-file-input');
+const jsonFileInput = document.getElementById('json-file-input');
 const importDictionaryBtn = document.getElementById('import-dictionary-file');
 const importDetailsBtn = document.getElementById('import-details-file');
+const importJsonBtn = document.getElementById('import-json-file');
 const exportJsonBtn = document.getElementById('export-json-file');
 
-let pstrings = {}
-let details = {}
+let pstrings = {};
+let details = {};
+
+function enableExportButton() {
+    const hasData = Array.isArray(details) ? details.length > 0 : Object.keys(details).length > 0;
+    if (hasData) {
+        exportJsonBtn.removeAttribute("disabled");
+    } else {
+        exportJsonBtn.addAttribute("disabled");
+    }
+}
 
 importDictionaryBtn.addEventListener('click', () => {
     dictionaryFileInput.click();
@@ -17,6 +28,10 @@ importDictionaryBtn.addEventListener('click', () => {
 
 importDetailsBtn.addEventListener('click', () => {
     detailsFileInput.click();
+});
+
+importJsonBtn.addEventListener('click', () => {
+    jsonFileInput.click();
 });
 
 dictionaryFileInput.addEventListener('change', (event) => {
@@ -27,11 +42,9 @@ dictionaryFileInput.addEventListener('change', (event) => {
 
     reader.onload = (e) => {
         const rawContent = e.target.result;
-        pstrings = msParser(rawContent);
-        
-        const output = JSON.stringify(pstrings, null, 2);
-        tabManager.addTab('dictionary-tab', file.name, output);
+        tabManager.addTab('dictionary-tab', "Base_Items.ms", rawContent);
 
+        pstrings = msParser(rawContent);
         importDetailsBtn.removeAttribute("disabled");
     };
 
@@ -47,11 +60,41 @@ detailsFileInput.addEventListener('change', (event) => {
     reader.onload = (e) => {
         const rawContent = e.target.result;
         details = texParser(rawContent, ParseDetailDict, pstrings);
-        
-        const output = JSON.stringify(details, null, 2);
-        tabManager.addTab('details-tab', file.name, output);
+        tabManager.addTab('details-tab', "Details.details", rawContent);
 
-        exportJsonBtn.removeAttribute("disabled");
+        const output = JSON.stringify(details, null, 2);
+        tabManager.addTab('json-data-tab', "JSON Data", output);
+
+        enableExportButton();
+    };
+
+    reader.readAsText(file);
+});
+
+jsonFileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        let jsonData;
+        try {
+            jsonData = JSON.parse(e.target.result);
+        } catch (err) {
+            alert("Invalid JSON file: " + err.message);
+            return;
+        }
+        details = jsonData;
+
+        const output = JSON.stringify(details, null, 2);
+        tabManager.addTab('json-data-tab', "JSON Data", output);
+
+        const { rawText, dictionary } = generateFiles(details, ParseDetailDict);
+        tabManager.addTab('details-tab', "Details.details", rawText);
+        tabManager.addTab('dictionary-tab', "Base_Items.ms", dictionary);
+
+        enableExportButton();
     };
 
     reader.readAsText(file);
